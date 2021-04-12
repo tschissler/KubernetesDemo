11d38d0aac5ff4b4ea5c97ab2d1ea87f99238eaf
Michael Raue <mich@el-raue.com>
2021-04-12 23:06:03 +0200

    Add brand new shiny Blazor Project for our UI

---
+++ PrimeDecompositionUi/Data/NumberGeneratorParameter.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimeDecompositionUi.Data
+{
+    public class NumberGeneratorParameter
+    {
+        [Required]
+        [Range(2, long.MaxValue, ErrorMessage = "Number should be between 2 and 9223372036854775807")]
+        public long MaxNumber { get; set; } = 5000;
+
+        [Required]
+        [Range(100, 10000, ErrorMessage = "Interval should be between 100 and 10000 ms")]
+        public long IntervalInMillis { get; set; } = 1000;
+    }
+}
