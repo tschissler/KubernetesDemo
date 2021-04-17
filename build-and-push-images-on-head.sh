adf015bea2d5c729bf86ec536825e448b81d80c7
Thomas Schissler <thomas@agilemax.de>
2021-04-18 00:15:59 +0200

    Added version as variable in sh script

--- build-and-push-images-on-head.sh
+++ build-and-push-images-on-head.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo docker build -t a1:32000/primedecomposition:1.0 -f PrimeDecomposition/Dockerfile .
-sudo docker build -t a1:32000/numbergenerator:1.0 -f NumberGenerator/Dockerfile .
-sudo docker build -t a1:32000/primedecompositionui:1.0 -f PrimeDecompositionUi/Dockerfile .
-sudo docker push a1:32000/primedecomposition:1.0 && \
-sudo docker push a1:32000/numbergenerator:1.0 && \
-sudo docker push a1:32000/primedecompositionui:1.0
+VERSION=1.4
+sudo docker build -t a1:32000/primedecomposition:$VERSION -f PrimeDecomposition/Dockerfile .
+sudo docker build -t a1:32000/numbergenerator:$VERSION -f NumberGenerator/Dockerfile .
+sudo docker build -t a1:32000/primedecompositionui:$VERSION -f PrimeDecompositionUi/Dockerfile .
+sudo docker push a1:32000/primedecomposition:$VERSION && \
+sudo docker push a1:32000/numbergenerator:$VERSION && \
+sudo docker push a1:32000/primedecompositionui:$VERSION
