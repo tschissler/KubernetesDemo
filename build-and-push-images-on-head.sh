e4c87388215cf70dfadc1cf5a97df4c33128c492
Michael Raue <mich@el-raue.com>
2021-04-18 21:46:31 +0200

    Make file more configurable

--- build-and-push-images-on-head.sh
+++ build-and-push-images-on-head.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 VERSION=1.4
-sudo docker build -t a1:32000/primedecomposition:$VERSION -f PrimeDecomposition/Dockerfile .
-sudo docker build -t a1:32000/numbergenerator:$VERSION -f NumberGenerator/Dockerfile .
-sudo docker build -t a1:32000/primedecompositionui:$VERSION -f PrimeDecompositionUi/Dockerfile .
-sudo docker push a1:32000/primedecomposition:$VERSION && \
-sudo docker push a1:32000/numbergenerator:$VERSION && \
-sudo docker push a1:32000/primedecompositionui:$VERSION
+CLUSTERNAME=a1
+sudo docker build -t $CLUSTERNAME:32000/primedecomposition:$VERSION -f PrimeDecomposition/Dockerfile .
+sudo docker build -t $CLUSTERNAME:32000/numbergenerator:$VERSION -f NumberGenerator/Dockerfile .
+sudo docker build -t $CLUSTERNAME:32000/primedecompositionui:$VERSION -f PrimeDecompositionUi/Dockerfile .
+sudo docker push $CLUSTERNAME:32000/primedecomposition:$VERSION && \
+sudo docker push $CLUSTERNAME:32000/numbergenerator:$VERSION && \
+sudo docker push $CLUSTERNAME:32000/primedecompositionui:$VERSION
